ccb3c17b9d8341b7b4d9a84bf81499b476c7e059
Jason McCarver <slam@parasite.cc>
2019-05-26 23:01:35 -0500

    Send blocks of pages to worker goroutines instead of individual pages -- decent speedup, especially against uncompressed wiki dump

--- extract_wordlist.go
+++ extract_wordlist.go
@@ -48,7 +48,7 @@ type Word struct {
 // This not-at-all correct parser is much faster
 // On my test system the runtime of this program on the wiktionary dump
 // ran in 1.5 minutes using xml and about .5 minutes this way
-func parsePage(cPage chan []byte, cResults chan map[string]*Word) {
+func parsePageBlock(cPage chan []byte, cResults chan map[string]*Word) {
     words := make(map[string]*Word, 1024)
     var elem *Word
     var ok bool
@@ -82,68 +82,70 @@ func parsePage(cPage chan []byte, cResults chan map[string]*Word) {
         return false
     }
 
-    NEXTPAGE:
     for {
-        data := <- cPage
-        if data == nil {
+        block := <- cPage
+        if block == nil {
             cResults <- words
             return
         }
 
-        title := bytes.SplitN(data, tagTitle, 2)
-        if len(title) != 2 {
-            continue NEXTPAGE
-        }
+        NEXTPAGE:
+        for _,data := range(bytes.SplitAfter(block, []byte("</page>"))) {
+            title := bytes.SplitN(data, tagTitle, 2)
+            if len(title) != 2 {
+                continue NEXTPAGE
+            }
 
-        title = bytes.SplitN(title[1], tagTitleEnd, 2)
-        if len(title) != 2 {
-            continue NEXTPAGE
-        }
+            title = bytes.SplitN(title[1], tagTitleEnd, 2)
+            if len(title) != 2 {
+                continue NEXTPAGE
+            }
 
-        word := string(title[0])
+            word := string(title[0])
 
-        if !rxValidWord.MatchString(word) {
-            continue NEXTPAGE
-        }
+            if !rxValidWord.MatchString(word) {
+                continue NEXTPAGE
+            }
 
-        revisions := bytes.Split(data, tagRevision)
-        if len(revisions) < 1 {
-            continue NEXTPAGE
-        }
+            revisions := bytes.Split(data, tagRevision)
+            if len(revisions) < 1 {
+                continue NEXTPAGE
+            }
 
-        text := bytes.SplitN(revisions[len(revisions)-1], tagText, 2)
-        if len(text) != 2 {
-            continue NEXTPAGE
-        }
+            text := bytes.SplitN(revisions[len(revisions)-1], tagText, 2)
+            if len(text) != 2 {
+                continue NEXTPAGE
+            }
 
-        text = bytes.SplitN(text[1], tagTextEnd, 2)
-        if len(text) != 2 {
-            continue NEXTPAGE
-        }
+            text = bytes.SplitN(text[1], tagTextEnd, 2)
+            if len(text) != 2 {
+                continue NEXTPAGE
+            }
 
-        if !wordOk(word, text[0]) {
-            continue NEXTPAGE
-        }
+            if !wordOk(word, text[0]) {
+                continue NEXTPAGE
+            }
 
-        if elem, ok = words[word]; !ok {
-            // This is the first time we've seen this word
-            // so we need to add it to our map
-            //elem = &Word{ p.Title, make(map[string]bool,1), false }
-            elem = &Word{ word: word, pluralOf: make(map[string]bool,1) }
-            words[word] = elem
-        }
+            if elem, ok = words[word]; !ok {
+                // This is the first time we've seen this word
+                // so we need to add it to our map
+                //elem = &Word{ p.Title, make(map[string]bool,1), false }
+                elem = &Word{ word: word, pluralOf: make(map[string]bool,1) }
+                words[word] = elem
+            }
 
-        // If it's a plural we need to track that, but we
-        // still need to add it to our word list since
-        // there are weird words like 'spices' which is
-        // a plural of 'spice' which is a plural of 'spouse'
-        //
-        // Also, there are words like petties that has two
-        // entries, one is 'petties' and one is 'Petties'
-        // with the Uppser case being the plural of a surname
-        match := rxPlural.FindSubmatch(text[0])
-        if len(match) > 1 && !strings.EqualFold(elem.word, string(match[1])) {
-            elem.pluralOf[string(match[1])] = true
+            // If it's a plural we need to track that, but we
+            // still need to add it to our word list since
+            // there are weird words like 'spices' which is
+            // a plural of 'spice' which is a plural of 'spouse'
+            //
+            // Also, there are words like petties that has two
+            // entries, one is 'petties' and one is 'Petties'
+            // with the Uppser case being the plural of a surname
+            match := rxPlural.FindSubmatch(text[0])
+            if len(match) > 1 && !strings.EqualFold(elem.word, string(match[1])) {
+                elem.pluralOf[string(match[1])] = true
+            }
         }
     }
 }
@@ -154,7 +156,7 @@ func splitPages(data []byte, atEOF bool) (advance int, token []byte, err error) 
         return 0, nil, nil
     }
 
-    found := bytes.Index(data, []byte("</page>"))
+    found := bytes.LastIndex(data, []byte("</page>"))
     if (found >= 0) {
         return found+7, data[0:found+7], nil
 
@@ -255,20 +257,26 @@ func main() {
 
     scanner := bufio.NewScanner(fh)
     scanner.Split(splitPages)
-    scanner.Buffer(make([]byte, 1024*1024), 1024*1024*10)
+    scanner.Buffer(make([]byte, 1024*1024*10), 1024*1024*100)
 
     for i:= 0; i < nrCPU; i++ {
-        go parsePage(cPage, cResults)
+        go parsePageBlock(cPage, cResults)
     }
 
     nrFound := 0
+    emptyCount := 0
     for scanner.Scan() {
         nrFound++
 
         page := make([]byte, len(scanner.Bytes()))
         copy(page, scanner.Bytes())
+        if len(cPage) == 0 {
+            emptyCount++
+        }
         cPage <- page
     }
+
+    log.Printf("Channel was empty %v times out of %v blocks (empty %.2f%%)\n", emptyCount, nrFound, (float64(emptyCount)/float64(nrFound))*100.0)
 
     if scanner.Err() != nil {
         log.Fatalf("scanner error: %v\n", scanner.Err().Error())
