a17b16421355b7cae74ac456e94d98d8fdad7c27
Jason McCarver <slam@parasite.cc>
2019-06-10 12:17:00 -0500

    set GO111MODULE so build works from within go workspace

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+# Allow this to work in your go workspace so you can
+# use go get
+export GO111MODULE=on
+
 fatal() {
     echo "ERROR: $*"
     exit 1
 }
 
-cd bin || fatal "No bin/ directory"
+cd bin || mkdir bin || fatal "No bin/ directory"
 for cmd in ../cmd/*; do go build $cmd; done
