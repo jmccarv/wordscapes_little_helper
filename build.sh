9a8aeae0f7a34d63a6d1da80a69fb27e546959ae
aray59 <aray59@bloomberg.net>
2019-06-13 18:14:23 +0200

    js => ts

--- build.sh
+++ build.sh
@@ -13,3 +13,7 @@ test -d bin || mkdir bin || fatal "Failed to create directory 'bin'"
 cd bin || fatal "Faled to switch to directory 'bin'"
 
 for cmd in ../cmd/*; do go build $cmd; done
+
+# lazy way to build ts
+cd ../www/js/ || fatal "Failed to switch to directory 'www/js'"
+./build.sh
